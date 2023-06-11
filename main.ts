eab1d9f7fb9ba93483f2611fbde49172f40ef6d1
clil972 <136168240+clil972@users.noreply.github.com>
2023-06-11 20:10:30 +0300

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,20 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    while (!(input.buttonIsPressed(Button.B))) {
+        SuperBit.MotorRunDual(
+        SuperBit.enMotors.M1,
+        50,
+        SuperBit.enMotors.M4,
+        50
+        )
+        basic.pause(2000)
+        SuperBit.MotorRunDual(
+        SuperBit.enMotors.M1,
+        -37,
+        SuperBit.enMotors.M1,
+        -37
+        )
+        basic.pause(500)
+    }
+    SuperBit.MotorStopAll()
+    basic.showIcon(IconNames.Heart)
 })
