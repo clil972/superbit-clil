44705e773bfbf9d5521affdb05a9a848469ab8e2
clil972 <136168240+clil972@users.noreply.github.com>
2023-06-11 20:19:19 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,19 +2,21 @@ input.onButtonPressed(Button.A, function () {
     while (!(input.buttonIsPressed(Button.B))) {
         SuperBit.MotorRunDual(
         SuperBit.enMotors.M1,
-        50,
+        86,
         SuperBit.enMotors.M4,
-        50
+        41
         )
         basic.pause(2000)
         SuperBit.MotorRunDual(
         SuperBit.enMotors.M1,
-        -37,
-        SuperBit.enMotors.M1,
-        -37
+        -86,
+        SuperBit.enMotors.M4,
+        -86
         )
         basic.pause(500)
     }
+})
+input.onButtonPressed(Button.B, function () {
     SuperBit.MotorStopAll()
     basic.showIcon(IconNames.Heart)
 })
